7d8486a0feb5494dffb27f66239fa8b7623fdc2c
binhphan <phanbinhfedev150504@gmail.com>
2025-09-24 14:20:41 +0700

    Lesson 5: Array Methods

---
+++ .history/lesson/lesson5/lesson5_20250924130624.js
@@ -0,0 +1,31 @@
+//? --------- Truy xuất các phần tử
+const arr = [10, 20, 30, 40, 50];
+
+// a) Lấy độ dài mảng
+console.log(arr.length); // Output: 5
+
+// b) Lấy phần tử cuối của mảng
+console.log(arr[arr.length - 1]); // Output: 50
+
+// c) Phương thức "at"
+console.log(arr.at(-1)); // Output: 50
+console.log(arr.at(-2)); // Output: 40
+
+// d) Phương thức findIndex
+console.log(arr.findIndex((num) => num > 30)); // Output: 2
+
+// e) Dùng for để in ra các phần tử
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// f) Dùng forEach để in ra các phần tử
+arr.forEach((item) => console.log(item));
+
+// g) Dùng for-of
+for (let item of arr) {
+  console.log(item);
+}
+
+// g) Phương thức find
+console.log(arr.find((num) => num > 28)); // Output: 30
---
+++ .history/lesson5/lesson5_20250924124250.js
@@ -0,0 +1,19 @@
+//? --------- Truy xuất các phần tử
+const arr = [10, 20, 30, 40, 50];
+
+// a) Lấy độ dài mảng
+
+// b) Lấy phần tử cuối của mảng
+
+// c) Phương thức "at"
+
+// d) Phương thức findIndex
+
+// e) Dùng for để in ra các phần tử
+for (let i = 0; i < length; i++) {
+  console.log(arr[i]);
+}
+
+// f) Dùng forEach để in ra các phần tử
+
+// g) Phương thức find
